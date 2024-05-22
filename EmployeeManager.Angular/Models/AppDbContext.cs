538b9b0ca5e0ff4d451449c807da687ecee1f90a
llsaimur <rashidsaimur@icloud.com>
2024-05-22 00:59:51 -0400

    Add README.md.

---
+++ EmployeeManager.Angular/Models/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManager.Angular.Security;
+using System.Diagnostics.Metrics;
+
+
+namespace EmployeeManager.Angular.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
